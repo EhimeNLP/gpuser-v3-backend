69bfba1c3930e6f7349501cbcabecf08da29df28
t-ohtsuka89 <102138393+t-ohtsuka89@users.noreply.github.com>
2024-01-11 01:12:18 +0000

    Add Flask-Cors for CORS support

--- application.py
+++ application.py
@@ -10,8 +10,10 @@ import paramiko
 from decouple import config
 from flask import Flask, jsonify
 from flask_caching import Cache
+from flask_cors import CORS
 
 app = Flask(__name__, instance_relative_config=True)
+cors = CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:3000"}})
 
 # 標準設定ファイル読み込み
 app.config.from_object("settings.DefaultConfig")
